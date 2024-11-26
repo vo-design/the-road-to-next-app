dad87724deb669f08d99fd0fd621bd18c7d2b930
Volodymyr O <hello@vdigital.design>
2024-11-26 18:38:43 +0200

    Animation with Tailwind

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -18,7 +18,7 @@ const TicketsPage = () => {
                 <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
                 <p className="text-sm text-muted-foreground">All your tickets in one place</p>
             </div>
-            <div className="flex-1 flex flex-col items-center gap-y-4">
+            <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
                 {initialTickets.map((ticket) => (
                     <div key={ticket.id} className="w-full max-w-[420px] p-4 border border-slate-100 rounded">
                         <div>{TICKET_ICONS[ticket.status]}</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ export default {
         foreground: "var(--foreground)",
       },
     },
+    keyframes: {
+      "fade-in-from-top": {
+        from: {opacity: "0", transform: "translateY(-20px)"},
+        to: {opacity: "1", transform: "translateY(0)"},
+      }
+    },
+    animation: {
+      "fade-in-from-top": "fade-in-from-top 0.5s ease-out",
+    }
   },
   plugins: [],
 } satisfies Config;
