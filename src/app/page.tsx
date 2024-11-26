97b771764cbc97c0a247a3a1de2b570e6bc57fa0
Volodymyr O <hello@vdigital.design>
2024-11-26 17:27:57 +0200

    Tailwind CSS in Next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,16 @@ import {ticketPaths} from "@/paths";
 
 const HomePage = () => {
   return (
-      <div>
-        <h2 className="text-lg">Home Page</h2>
-        <Link href={ticketPaths()} className="underlined">
-          Go to tickets
-        </Link>
+      <div className="flex-1 flex flex-col gap-y-8">
+        <div>
+          <h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
+            <p className="text-sm text-muted-foreground">Your home place to start</p>
+        </div>
+        <div className="flex-1 flex flex-col items-center">
+            <Link href={ticketPaths()} className="underlined">
+                Go to tickets
+            </Link>
+        </div>
       </div>
   );
 };
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -12,16 +12,24 @@ const TICKET_ICONS = {
 
 const TicketsPage = () => {
     return (
-        <div>
-            {initialTickets.map((ticket) => (
-                <div key={ticket.id}>
-                    <div>{TICKET_ICONS[ticket.status]}</div>
-                    <h2 className="text-lg">{ticket.title}</h2>
-                    <Link href={ticketPath(String(ticket.id))} className="text-sm underline">
-                        View
-                    </Link>
-                </div>
-            ))}
+        <div className="flex-1 flex flex-col gap-y-8">
+            <div>
+                <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
+                <p className="text-sm text-muted-foreground">All your tickets in one place</p>
+            </div>
+            <div className="flex-1 flex flex-col items-center gap-y-4">
+                {initialTickets.map((ticket) => (
+                    <div key={ticket.id} className="w-full max-w-[420px] p-4 border border-slate-100 rounded">
+                        <div>{TICKET_ICONS[ticket.status]}</div>
+                        <h3 className="text-lg truncate font-semibold">{ticket.title}</h3>
+                        <p className="text-sm text-slate-500 truncate">{ticket.content}</p>
+                        <Link href={ticketPath(String(ticket.id))} className="text-sm underline">
+                            View
+                        </Link>
+                    </div>
+                ))}
+            </div>
+
         </div>
 
     );
