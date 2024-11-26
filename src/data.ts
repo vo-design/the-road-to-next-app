ec2f2b88a202872d6e0f51fde5727227ec08bdf3
Volodymyr O <hello@vdigital.design>
2024-11-26 15:47:53 +0200

    const Assertions

--- src/data.ts
+++ src/data.ts
@@ -3,12 +3,18 @@ export const initialTickets = [
         id: 1,
         title: "Ticket 1",
         content: "This is a Ticket 1",
-        status: "DONE",
+        status: "DONE" as const,
     },
     {
         id: 2,
         title: "Ticket 2",
         content: "This is a Ticket 2",
-        status: "OPEN",
+        status: "OPEN" as const,
+    },
+    {
+        id: 3,
+        title: "Ticket 3",
+        content: "This is a Ticket 3",
+        status: "IN_PROGRESS" as const,
     }
 ];
