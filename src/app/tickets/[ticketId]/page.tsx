423610f34bd42fb50b6c32e64dada81944eb3a38
Volodymyr O <hello@vdigital.design>
2024-12-01 17:41:30 +0200

    Placeholder

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,34 +1,50 @@
-import {initialTickets} from "@/data";
+import Link from "next/link";
+
+import { Placeholder } from "@/components/Placeholder";
+import { Button } from "@/components/ui/button";
+import { initialTickets } from "@/data";
+import { ticketPaths } from "@/paths";
 
 type TicketPageProps = {
-    params: Promise<{
-        ticketId: string;
-    }>;
+  params: Promise<{
+    ticketId: string;
+  }>;
 };
 
 const TicketPage = async ({ params }: TicketPageProps) => {
-    let ticketId: string;
+  let ticketId: string;
 
-    try {
-        const { ticketId: id } = await params;
-        ticketId = id; // For additional use if needed
-    } catch (error) {
-        console.error("Error fetching ticket ID:", error);
-        return <div>Unable to load ticket. Please try again later.</div>;
-    }
+  try {
+    const { ticketId: id } = await params;
+    ticketId = id; // For additional use if needed
+  } catch (error) {
+    console.error("Error fetching ticket ID:", error);
+    return <div>Unable to load ticket. Please try again later.</div>;
+  }
 
-    const ticket = initialTickets.find((ticket) => String(ticket.id) === ticketId);
+  const ticket = initialTickets.find(
+    (ticket) => String(ticket.id) === ticketId,
+  );
 
-    if (!ticket) {
-        return <div>Ticket not found</div>;
-    }
-
+  if (!ticket) {
     return (
-        <div>
-            <h2 className="text-lg">{ticket.title}</h2>
-            <p className="text-sm">{ticket.content}</p>
-        </div>
+      <Placeholder
+        label="Ticket not found"
+        button={
+          <Button asChild variant="outline">
+            <Link href={ticketPaths()}>Go to tickets</Link>
+          </Button>
+        }
+      />
     );
+  }
+
+  return (
+    <div>
+      <h2 className="text-lg">{ticket.title}</h2>
+      <p className="text-sm">{ticket.content}</p>
+    </div>
+  );
 };
 
 export default TicketPage;
---
+++ src/components/Placeholder.tsx
@@ -0,0 +1,26 @@
+import { MessageSquareWarning } from "lucide-react";
+import { cloneElement } from "react";
+
+type PlaceholderProps = {
+  label: string;
+  icon?: React.ReactElement;
+  button?: React.ReactNode;
+};
+
+const Placeholder = ({
+  label,
+  icon = <MessageSquareWarning />,
+  button = null,
+}: PlaceholderProps) => {
+  return (
+    <div className="flex-1 self-center flex flex-col items-center justify-center gap-y-2">
+      {cloneElement(icon, {
+        className: "w-16 h-16",
+      })}
+      <h2 className="text-lg text-center">{label}</h2>
+      {button}
+    </div>
+  );
+};
+
+export { Placeholder };
