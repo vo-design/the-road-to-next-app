e0e5b339e41f8f775a6cb0b37062987d5488d798
Volodymyr O <hello@vdigital.design>
2024-12-03 16:15:53 +0200

    Reuse

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -4,6 +4,7 @@ import { Placeholder } from "@/components/Placeholder";
 import { Button } from "@/components/ui/button";
 import { initialTickets } from "@/data";
 import { ticketPaths } from "@/paths";
+import { TicketItem } from "@/features/ticket/components/ticket-item";
 
 type TicketPageProps = {
   params: Promise<{
@@ -38,9 +39,8 @@ const TicketPage = async ({ params }: TicketPageProps) => {
   }
 
   return (
-    <div>
-      <h2 className="text-lg">{ticket.title}</h2>
-      <p className="text-sm">{ticket.content}</p>
+    <div className="flex justify-center animate-fade-in-from-top">
+      <TicketItem ticket={ticket} isDetail />
     </div>
   );
 };
--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,3 +1,5 @@
+import { clsx } from "clsx";
+
 import { SquareArrowOutUpRight } from "lucide-react";
 import Link from "next/link";
 
@@ -9,9 +11,10 @@ import { ticketPath } from "@/paths";
 
 type TicketItemProps = {
   ticket: Ticket;
+  isDetail?: boolean;
 };
 
-const TicketItem = ({ ticket }: TicketItemProps) => {
+const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
   const detailButton = (
     <Button variant="outline" size="icon" asChild>
       <Link href={ticketPath(String(ticket.id))}>
@@ -21,7 +24,12 @@ const TicketItem = ({ ticket }: TicketItemProps) => {
   );
 
   return (
-    <div className="w-full max-w-[420px] flex gap-x-1">
+    <div
+      className={clsx("w-full flex gap-x-1", {
+        "max-w-[580px]": isDetail,
+        "max-w-[420px]": !isDetail,
+      })}
+    >
       <Card className="w-full">
         <CardHeader>
           <CardTitle className="flex gap-x-2">
@@ -30,12 +38,18 @@ const TicketItem = ({ ticket }: TicketItemProps) => {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <span className="line-clamp-3 whitespace-break-spaces">
+          <span
+            className={clsx("whitespace-break-spaces", {
+              "line-clamp-3": !isDetail,
+            })}
+          >
             {ticket.content}
           </span>
         </CardContent>
       </Card>
-      <div className="flex flex-col gap-y-1">{detailButton}</div>
+      {isDetail ? null : (
+        <div className="flex flex-col gap-y-1">{detailButton}</div>
+      )}
     </div>
   );
 };
