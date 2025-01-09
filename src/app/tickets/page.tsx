49ffa33c4f0cbb02edd3743753376cf78118b0e9
Volodymyr O <hello@vdigital.design>
2025-01-09 14:35:05 +0200

    Dynamic rendering

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,31 +1,32 @@
-
-
 import {Suspense} from "react";
+import {ErrorBoundary} from "react-error-boundary";
 
-import { Heading } from "@/components/heading";
+import {Heading} from "@/components/heading";
+import {Placeholder} from "@/components/Placeholder";
 import {Spinner} from "@/components/spinner";
 import {TicketList} from "@/features/ticket/components/ticket-list";
-import {ErrorBoundary} from "react-error-boundary";
-import {Placeholder} from "@/components/Placeholder";
+
+export const dynamic = "force-dynamic";
+
 
 const TicketsPage = () => {
 
 
-  return (
-    <div className="flex-1 flex flex-col gap-y-8">
-      <Heading
-        title="Tickets Page"
-        description="All your tickets in one place"
-      />
-        <ErrorBoundary fallback={<Placeholder label="Something went wrong!"/>}>
-            <Suspense fallback={<Spinner />}>
-                <TicketList />
-            </Suspense>
-        </ErrorBoundary>
+    return (
+        <div className="flex-1 flex flex-col gap-y-8">
+            <Heading
+                title="Tickets Page"
+                description="All your tickets in one place"
+            />
+            <ErrorBoundary fallback={<Placeholder label="Something went wrong!"/>}>
+                <Suspense fallback={<Spinner/>}>
+                    <TicketList/>
+                </Suspense>
+            </ErrorBoundary>
 
 
-    </div>
-  );
+        </div>
+    );
 };
 
 export default TicketsPage;
