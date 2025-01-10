a866444b4e00bb7981bcadc71872eb89fcf0aa3c
Volodymyr O <hello@vdigital.design>
2025-01-10 17:46:49 +0200

    Fix imports

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,12 +1,12 @@
 import {Suspense} from "react";
 import {ErrorBoundary} from "react-error-boundary";
 
+import {CardCompact} from "@/components/card-compact";
 import {Heading} from "@/components/heading";
 import {Placeholder} from "@/components/Placeholder";
 import {Spinner} from "@/components/spinner";
 import {TicketCreateForm} from "@/features/ticket/components/ticket-create-form";
 import {TicketList} from "@/features/ticket/components/ticket-list";
-import {CardCompact} from "@/components/card-compact";
 
 
 const TicketsPage = () => {
--- src/features/ticket/components/ticket-create-form.tsx
+++ src/features/ticket/components/ticket-create-form.tsx
@@ -2,6 +2,7 @@ import {Button} from "@/components/ui/button";
 import {Input} from "@/components/ui/input";
 import {Label} from "@/components/ui/label";
 import {Textarea} from "@/components/ui/textarea";
+
 import {createTicket} from "../actions/create-ticket";
 
 const TicketCreateForm = () => {
