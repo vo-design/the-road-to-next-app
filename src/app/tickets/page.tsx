19bea9c13a211bc6e70b77b231e11c645ddcff01
Volodymyr O <hello@vdigital.design>
2025-01-10 17:46:06 +0200

    Configuration over Composition

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -4,15 +4,9 @@ import {ErrorBoundary} from "react-error-boundary";
 import {Heading} from "@/components/heading";
 import {Placeholder} from "@/components/Placeholder";
 import {Spinner} from "@/components/spinner";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
 import {TicketCreateForm} from "@/features/ticket/components/ticket-create-form";
 import {TicketList} from "@/features/ticket/components/ticket-list";
+import {CardCompact} from "@/components/card-compact";
 
 
 const TicketsPage = () => {
@@ -24,15 +18,14 @@ const TicketsPage = () => {
                 title="Tickets Page"
                 description="All your tickets in one place"
             />
-            <Card className="w-full max-w-[420px] self-center">
-                <CardHeader>
-                    <CardTitle>Create Ticket</CardTitle>
-                    <CardDescription>A new ticket will be created</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <TicketCreateForm/>
-                </CardContent>
-            </Card>
+
+            <CardCompact
+                title="Create Ticket"
+                description="A new ticket will be created"
+                className="w-full max-w-[420px] self-center"
+                content={<TicketCreateForm/>}
+            />
+
             <ErrorBoundary fallback={<Placeholder label="Something went wrong!"/>}>
                 <Suspense fallback={<Spinner/>}>
                     <TicketList/>
