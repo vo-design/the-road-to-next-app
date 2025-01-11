bc6e48dafb61d34d22d48513c3923da5523cf597
Volodymyr O <hello@vdigital.design>
2025-01-11 17:32:59 +0200

    Update abstractions

--- src/app/tickets/[ticketId]/edit/page.tsx
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -2,7 +2,7 @@ import {notFound} from "next/navigation";
 
 import {CardCompact} from "@/components/card-compact";
 import {getTicket} from "@/features/queries/get-ticket";
-import {TicketUpdateForm} from "@/features/ticket/components/ticket-update-form";
+import {TicketUpsertForm} from "@/features/ticket/components/ticket-upsert-form";
 
 type TicketEditPageProps = {
     params: Promise<{
@@ -24,7 +24,7 @@ const TicketEditPage = async ({params}: TicketEditPageProps) => {
                 title="Edit Ticket"
                 description="Edit an existing ticket"
                 className="w-full max-w-[420px] animate-fade-from-top"
-                content={<TicketUpdateForm ticket={ticket}/>}
+                content={<TicketUpsertForm ticket={ticket}/>}
             />
         </div>
     );
