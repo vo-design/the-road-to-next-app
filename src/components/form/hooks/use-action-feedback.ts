cdd039ce6e8a0e59702ed139aa890d5b963c9f3b
Volodymyr O <hello@vdigital.design>
2025-01-18 13:24:19 +0200

    Debugging () Software Craftsmanship

--- src/components/form/hooks/use-action-feedback.ts
+++ src/components/form/hooks/use-action-feedback.ts
@@ -1,4 +1,4 @@
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 
 import {ActionState} from "@/components/form/utils/to-action-state";
 
@@ -15,7 +15,12 @@ const useActionFeedback = (
     actionState: ActionState,
     options: UseActionFeedbackOptions
 ) => {
+    const prevTimestamp = useRef(actionState.timestamp);
+    const isUpdate = prevTimestamp.current !== actionState.timestamp;
+
     useEffect(() => {
+        if (!isUpdate) return;
+
         if (actionState.status === "SUCCESS") {
             options.onSuccess?.({actionState});
         }
@@ -23,7 +28,8 @@ const useActionFeedback = (
         if (actionState.status === "ERROR") {
             options.onError?.({actionState});
         }
-    }, [actionState, options]);
+        prevTimestamp.current = actionState.timestamp;
+    }, [isUpdate, actionState, options]);
 }
 
 export {useActionFeedback};
