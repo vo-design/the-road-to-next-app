0724cd1a90f2d5f7ea365ead5a02154103161730
Volodymyr O <hello@vdigital.design>
2025-01-21 15:38:01 +0200

    Close DatePicker

--- src/components/data-picker.tsx
+++ src/components/data-picker.tsx
@@ -18,11 +18,17 @@ const DatePicker = ({id, name, defaultValue}: DatePickerProps) => {
     const [date, setDate] = useState<Date | undefined>(
         defaultValue ? new Date(defaultValue) : new Date()
     );
+    const [open, setOpen] = useState(false);
+
+    const handleSelect = (selectedDate: Date | undefined) => {
+        setDate(selectedDate);
+        setOpen(false);
+    };
 
     const formattedStringDate = date ? format(date, "yyyy-MM-dd") : "";
 
     return (
-        <Popover>
+        <Popover open={open} onOpenChange={setOpen}>
             <PopoverTrigger id={id} className="w-full" asChild>
                 <Button
                     variant="outline"
@@ -37,7 +43,7 @@ const DatePicker = ({id, name, defaultValue}: DatePickerProps) => {
                 <Calendar
                     mode="single"
                     selected={date}
-                    onSelect={setDate}
+                    onSelect={handleSelect}
                     initialFocus
                 />
             </PopoverContent>
