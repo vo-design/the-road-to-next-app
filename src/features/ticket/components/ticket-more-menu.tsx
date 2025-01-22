47c3aae78236b0525c3de6233c4efa88f9fb350c
Volodymyr O <hello@vdigital.design>
2025-01-22 15:49:38 +0200

    Ticket status (Read)

--- src/features/ticket/components/ticket-more-menu.tsx
+++ src/features/ticket/components/ticket-more-menu.tsx
@@ -1,34 +1,51 @@
-import { Ticket } from "@prisma/client";
-import { LucideTrash } from "lucide-react";
+import {Ticket, TicketStatus} from "@prisma/client";
+import {Trash} from "lucide-react";
 
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+
+import { TICKET_STATUS_LABELS } from "../constants";
 
 type TicketMoreMenuProps = {
     ticket: Ticket;
     trigger: React.ReactElement;
 };
 
-const TicketMoreMenu = ({ ticket, trigger }: TicketMoreMenuProps) => {
+const TicketMoreMenu = ({ticket, trigger}: TicketMoreMenuProps) => {
     const deleteButton = (
         <DropdownMenuItem>
-            <LucideTrash className="h-4 w-4" />
+            <Trash className="h-4 w-4"/>
             <span>Delete</span>
         </DropdownMenuItem>
+    );
+
+    const ticketStatusRadioGroupItems = (
+        <DropdownMenuRadioGroup value={ticket.status}>
+            {(Object.keys(TICKET_STATUS_LABELS) as Array<TicketStatus>).map((key) => (
+                <DropdownMenuRadioItem key={key} value={key}>
+                    {TICKET_STATUS_LABELS[key]}
+                </DropdownMenuRadioItem>
+            ))}
+        </DropdownMenuRadioGroup>
     );
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
             <DropdownMenuContent className="w-56" side="right">
+                {ticketStatusRadioGroupItems}
+                <DropdownMenuSeparator/>
                 {deleteButton}
             </DropdownMenuContent>
         </DropdownMenu>
     );
 };
 
-export { TicketMoreMenu };
+export {TicketMoreMenu};
--- src/features/ticket/constants.tsx
+++ src/features/ticket/constants.tsx
@@ -1,7 +1,13 @@
 import {CircleCheck, FolderOpen, LoaderCircle} from "lucide-react";
 
 export const TICKET_ICONS = {
-    OPEN: <FolderOpen />,
-    IN_PROGRESS: <LoaderCircle />,
-    DONE: <CircleCheck />,
+    OPEN: <FolderOpen/>,
+    IN_PROGRESS: <LoaderCircle/>,
+    DONE: <CircleCheck/>,
+};
+
+export const TICKET_STATUS_LABELS = {
+    OPEN: "Open",
+    DONE: "Done",
+    IN_PROGRESS: "In Progress",
 };
