befe2c00d4c611bb7a6b200d412979926379b8ea
Volodymyr O <hello@vdigital.design>
2025-01-29 14:24:05 +0200

    Non-Nullable Relation

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -5,8 +5,9 @@ import {redirect} from "next/navigation";
 import {z} from "zod";
 import {setCookieByKey} from "@/actions/cookies";
 import {ActionState, fromErrorToActionState, toActionState,} from "@/components/form/utils/to-action-state";
+import {getAuth} from "@/features/auth/queries/get-auth";
 import {prisma} from "@/lib/prisma";
-import {ticketPath, ticketsPath} from "@/paths";
+import {signInPath, ticketPath, ticketsPath} from "@/paths";
 import {toCent} from "@/utils/currency";
 
 const upsertTicketSchema = z.object({
@@ -21,6 +22,12 @@ export const upsertTicket = async (
     _actionState: ActionState,
     formData: FormData
 ) => {
+    const {user} = await getAuth();
+
+    if (!user) {
+        redirect(signInPath());
+    }
+
     try {
         const data = upsertTicketSchema.parse({
             title: formData.get("title"),
@@ -31,6 +38,7 @@ export const upsertTicket = async (
 
         const dbData = {
             ...data,
+            userId: user.id,
             bounty: toCent(data.bounty),
         };
 
