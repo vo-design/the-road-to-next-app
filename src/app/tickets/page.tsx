32aedba3a68903c89cf399b7e10834ab85db9fb2
Volodymyr O <hello@vdigital.design>
2025-01-30 15:23:17 +0200

    Protected Routes

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,23 +1,14 @@
 import {Suspense} from "react";
-import {ErrorBoundary} from "react-error-boundary";
 import {CardCompact} from "@/components/card-compact";
 import {Heading} from "@/components/heading";
-import {Placeholder} from "@/components/placeholder";
 import {Spinner} from "@/components/spinner";
 import {TicketList} from "@/features/ticket/components/ticket-list";
 import {TicketUpsertForm} from "@/features/ticket/components/ticket-upsert-form";
 
-
-const TicketsPage = () => {
-
-
+const TicketsPage = async () => {
     return (
-
         <div className="flex-1 flex flex-col gap-y-8">
-            <Heading
-                title="Tickets Page"
-                description="All your tickets in one place"
-            />
+            <Heading title="Tickets" description="All your tickets at one place"/>
 
             <CardCompact
                 title="Create Ticket"
@@ -26,14 +17,10 @@ const TicketsPage = () => {
                 content={<TicketUpsertForm/>}
             />
 
-            <ErrorBoundary fallback={<Placeholder label="Something went wrong!"/>}>
-                <Suspense fallback={<Spinner/>}>
-                    <TicketList/>
-                </Suspense>
-            </ErrorBoundary>
+            <Suspense fallback={<Spinner/>}>
+                <TicketList/>
+            </Suspense>
         </div>
-
-
     );
 };
 
