0b957b685648a2ea4793c7bf2ac904e86c09a9ce
Volodymyr O <hello@vdigital.design>
2025-02-04 10:19:59 +0200

    Breafcrumbs

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,6 +1,9 @@
-import { notFound } from "next/navigation";
-import { TicketItem } from "@/features/ticket/components/ticket-item";
-import { getTicket } from "@/features/ticket/queries/get-ticket";
+import {notFound} from "next/navigation";
+import {Breadcrumbs} from "@/components/breadcrumbs";
+import {Separator} from "@/components/ui/separator";
+import {TicketItem} from "@/features/ticket/components/ticket-item";
+import {getTicket} from "@/features/ticket/queries/get-ticket";
+import {homePath} from "@/paths";
 
 type TicketPageProps = {
     params: Promise<{
@@ -8,8 +11,8 @@ type TicketPageProps = {
     }>;
 };
 
-const TicketPage = async ({ params }: TicketPageProps) => {
-    const { ticketId } = await params;
+const TicketPage = async ({params}: TicketPageProps) => {
+    const {ticketId} = await params;
     const ticket = await getTicket(ticketId);
 
     if (!ticket) {
@@ -17,8 +20,19 @@ const TicketPage = async ({ params }: TicketPageProps) => {
     }
 
     return (
-        <div className="flex justify-center animate-fade-from-top">
-            <TicketItem ticket={ticket} isDetail />
+        <div className="flex-1 flex flex-col gap-y-8">
+            <Breadcrumbs
+                breadcrumbs={[
+                    {title: "Tickets", href: homePath()},
+                    {title: ticket.title},
+                ]}
+            />
+
+            <Separator/>
+
+            <div className="flex justify-center animate-fade-from-top">
+                <TicketItem ticket={ticket} isDetail/>
+            </div>
         </div>
     );
 };
