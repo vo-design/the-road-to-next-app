225d4a475acbf8057104cee21e3fff64214146e6
Volodymyr O <hello@vdigital.design>
2025-02-10 16:29:50 +0200

    Database Transactions

--- src/features/ticket/queries/get-tickets.tsx
+++ src/features/ticket/queries/get-tickets.tsx
@@ -16,25 +16,26 @@ export const getTickets = async (
     const skip = searchParams.size * searchParams.page;
     const take = searchParams.size;
 
-    const tickets = await prisma.ticket.findMany({
-        where,
-        skip,
-        take,
-        orderBy: {
-            [searchParams.sortKey]: searchParams.sortValue,
-        },
-        include: {
-            user: {
-                select: {
-                    username: true,
+    const [tickets, count] = await prisma.$transaction([
+        prisma.ticket.findMany({
+            where,
+            skip,
+            take,
+            orderBy: {
+                [searchParams.sortKey]: searchParams.sortValue,
+            },
+            include: {
+                user: {
+                    select: {
+                        username: true,
+                    },
                 },
             },
-        },
-    });
-
-    const count = await prisma.ticket.count({
-        where,
-    });
+        }),
+        prisma.ticket.count({
+            where,
+        }),
+    ]);
 
     return {
         list: tickets,
