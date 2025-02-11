bc030e59c46bcf460e78ea353ca0773bfddfcc92
Volodymyr O <hello@vdigital.design>
2025-02-11 13:47:51 +0200

    Dynamic page size

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,4 +1,5 @@
 import {Button} from "./ui/button";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue,} from "./ui/select";
 
 type PageAndSize = {
     page: number;
@@ -33,6 +34,10 @@ const Pagination = ({
         onPagination({...pagination, page: pagination.page + 1});
     };
 
+    const handleChangeSize = (size: string) => {
+        onPagination({page: 0, size: parseInt(size)});
+    };
+
     const previousButton = (
         <Button
             variant="outline"
@@ -55,10 +60,29 @@ const Pagination = ({
         </Button>
     );
 
+    const sizeButton = (
+        <Select
+            onValueChange={handleChangeSize}
+            defaultValue={pagination.size.toString()}
+        >
+            <SelectTrigger className="h-[36px]">
+                <SelectValue/>
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="5">5</SelectItem>
+                <SelectItem value="10">10</SelectItem>
+                <SelectItem value="25">25</SelectItem>
+                <SelectItem value="50">50</SelectItem>
+                <SelectItem value="100">100</SelectItem>
+            </SelectContent>
+        </Select>
+    );
+
     return (
         <div className="flex justify-between items-center">
             <p className="text-sm text-muted-foreground">{label}</p>
             <div className="flex gap-x-2">
+                {sizeButton}
                 {previousButton}
                 {nextButton}
             </div>
