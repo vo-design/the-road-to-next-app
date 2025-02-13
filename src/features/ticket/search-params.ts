844c1ba38018e082a15e102436416ddc6a7d0288
Volodymyr O <hello@vdigital.design>
2025-02-13 10:28:46 +0200

    Fix pagination

--- src/features/ticket/search-params.ts
+++ src/features/ticket/search-params.ts
@@ -17,7 +17,7 @@ export const sortOptions = {
 
 export const paginationParser = {
     page: parseAsInteger.withDefault(0),
-    size: parseAsInteger.withDefault(2),
+    size: parseAsInteger.withDefault(5),
 };
 
 export const paginationOptions = {
