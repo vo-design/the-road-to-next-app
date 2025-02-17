97c7d00911993cdb0d542b1781a07578416f36de
Volodymyr Onishchenko <hello@vdigital.design>
2025-02-17 13:33:52 +0200

    TypeScript Generic

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,3 +1,4 @@
+import {PaginatedData} from "@/types/pagination";
 import {Button} from "./ui/button";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue,} from "./ui/select";
 
@@ -9,10 +10,7 @@ type PageAndSize = {
 type PaginationProps = {
     pagination: PageAndSize;
     onPagination: (pagination: PageAndSize) => void;
-    paginatedMetadata: {
-        count: number;
-        hasNextPage: boolean;
-    };
+    paginatedMetadata: PaginatedData<unknown>["metadata"];
 };
 
 const Pagination = ({
--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -3,6 +3,7 @@
 import {useState} from "react";
 import {CardCompact} from "@/components/card-compact";
 import {Button} from "@/components/ui/button";
+import {PaginatedData} from "@/types/pagination";
 import {getComments} from "../queries/get-comments";
 import {CommentWithMetadata} from "../types";
 import {CommentCreateForm} from "./comment-create-form";
@@ -11,10 +12,7 @@ import {CommentItem} from "./comment-item";
 
 type CommentsProps = {
     ticketId: string;
-    paginatedComments: {
-        list: CommentWithMetadata[];
-        metadata: { count: number; hasNextPage: boolean; cursor?: string };
-    };
+    paginatedComments: PaginatedData<CommentWithMetadata>;
 };
 
 const Comments = ({ticketId, paginatedComments}: CommentsProps) => {
